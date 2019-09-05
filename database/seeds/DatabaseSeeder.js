67730da01d7ebc8fbc5136a7c44e9a3f0bf4d10b
Bruno Carlos Mota Araujo <BrunoAraujo@caesb.gov.br>
2019-09-05 19:25:01 -0300

    Create Permissions

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -11,18 +11,56 @@
 */
 const User = use("App/Models/User");
 
+const Role = use("Adonis/Acl/Role");
+const Permission = use("Adonis/Acl/Permission");
+
 class DatabaseSeeder {
   async run() {
     const user = await User.create({
-      name: "Bruno Araujo",
-      email: "bruno.araujodf2@gmail.com",
-      password: '123456'
+      name: "Bruno",
+      email: "bruno@rocketseat.com",
+      password: "123456"
     });
 
-    await user.teams().create({
-      name: "Rockeseat",
-      user_id: user.id,
+    const createInvite = await Permission.create({
+      slug: "invites_create",
+      name: "Convidar membros"
     });
+
+    const createProject = await Permission.create({
+      slug: "projects_create",
+      name: "Criar projetos"
+    });
+
+    const admin = await Role.create({
+      slug: "administrator",
+      name: "administrador"
+    });
+
+    const moderator = await Role.create({
+      slug: "moderator",
+      name: "moderador"
+    });
+
+    await Role.create({
+      slug: "visitor",
+      name: "visitante"
+    });
+
+    await admin.permissions().attach([createInvite.id, createProject.id]);
+    await moderator.permissions().attach([createProject.id]);
+
+    const team = await user.teams().create({
+      name: "rockeseat",
+      user_id: user.id
+    });
+
+    const teamJoin = await user
+      .teamJoins()
+      .where("team_id", team.id)
+      .first();
+
+    await teamJoin.roles().attach([admin.id]);
   }
 }
 
